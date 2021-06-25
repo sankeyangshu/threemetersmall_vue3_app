8d6dec4aa21225551ec2f9bc9d24692881d1d066
王振 <1260323835@qq.com@example.com>
2021-06-25 10:22:28 +0800

    style(styles): 添加全局基础样式

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,7 +3,7 @@
  * @Author: 王振
  * @Date: 2021-06-25 10:02:21
  * @LastEditors: 王振
- * @LastEditTime: 2021-06-25 10:03:07
+ * @LastEditTime: 2021-06-25 10:14:09
  */
 module.exports = {
   plugins: {

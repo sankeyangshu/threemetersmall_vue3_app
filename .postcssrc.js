ea0ea124766ff6dcc178d5fc1b6fbcb54c3a775a
王振 <1260323835@qq.com@example.com>
2021-06-25 10:09:10 +0800

    build(deps): 添加移动端适配和vant组件库依赖

---
+++ .postcssrc.js
@@ -0,0 +1,33 @@
+/*
+ * @Description: px转rem文件
+ * @Author: 王振
+ * @Date: 2021-06-25 10:02:21
+ * @LastEditors: 王振
+ * @LastEditTime: 2021-06-25 10:03:07
+ */
+module.exports = {
+  plugins: {
+    // 配置使用 autoprefixer 插件
+    // 作用：生成浏览器 CSS 样式规则前缀
+    // VueCLI 内部已经配置了 autoprefixer 插件
+    // 所以又配置了一次，所以产生冲突了 所以需要删掉 'autoprefixer'
+    //'autoprefixer': {
+    //browsers: ['Android >= 4.0', 'iOS >= 8']
+    //},
+    // 配置使用 postcss-pxtorem 插件
+    // 作用：把 px 转为 rem
+    "postcss-pxtorem": {
+      //通过查阅文档我们可以看到 rootValue 支持两种参数类型：
+      //数字：固定值
+      //函数：动态计算返回
+      //postcss-pxtorem 处理每个 CSS 文件的时候都会来调用这个函数
+      //它会把被处理的 CSS 文件相关的信息通过参数传递给该函数
+      //rootValue: 37.5,
+      rootValue({ file }) {
+        //将 vant 组件 rem 设置成 37.5  自己写的样式设置为 75 这样就不需要考虑二倍图 除二了
+        return file.indexOf("vant") !== -1 ? 37.5 : 75;
+      },
+      propList: ["*"],
+    },
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
+/*
+ * @Description:项目入口文件
+ * @Author: 王振
+ * @Date: 2021-06-25 09:44:20
+ * @LastEditors: 王振
+ * @LastEditTime: 2021-06-25 10:01:39
+ */
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "normalize.css"; //统一各个浏览器之间的显示差异
+import "lib-flexible/flexible"; //移动端分辨率适配—rem
+import "vant/lib/index.css"; //全局导入vant
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App); //创建实例
+
+app.use(store);
+app.use(router);
+app.mount("#app");

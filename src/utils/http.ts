2e54ba318eb5e284ab62410b51a5752e113b7785
王振 <1260323835@qq.com@example.com>
2021-06-25 12:34:12 +0800

    feat(utils): 新增axios请求库和封装方法

---
+++ src/utils/http.ts
@@ -0,0 +1,80 @@
+/*
+ * @Description: 封装api请求方法
+ * @Author: 王振
+ * @Date: 2021-06-25 12:33:08
+ * @LastEditors: 王振
+ * @LastEditTime: 2021-06-25 12:33:26
+ */
+
+//1.导入封装好的axios实例
+import request from "./request";
+
+//包裹请求方法的容器
+const http = {
+  /**
+   * @description: 封装get请求方法
+   * @param {string} url 请求地址
+   * @param {any} params 请求参数
+   */
+  get(url: string, params?: any) {
+    const config: any = {
+      method: "get", // `method` 是创建请求时使用的方法
+      url: url, // `url` 是用于请求的服务器 URL
+    };
+    if (params) {
+      config.params = params;
+    }
+    return request(config);
+  },
+
+  /**
+   * @description: 封装post方法
+   * @param {string} url 请求地址
+   * @param {any} params 请求参数
+   */
+  post(url: string, params: any) {
+    const config: any = {
+      method: "post",
+      url: url,
+    };
+    if (params) {
+      config.data = params;
+    }
+    return request(config);
+  },
+
+  /**
+   * @description: 封装patch方法
+   * @param {string} url 请求地址
+   * @param {any} params 请求参数
+   */
+  patch(url: string, params: any) {
+    const config: any = {
+      method: "put",
+      url: url,
+    };
+    if (params) {
+      config.params = params;
+    }
+    return request(config);
+  },
+
+  /**
+   * @description: 封装delete方法
+   * @param {string} url 请求地址
+   * @param {any} params 请求参数
+   */
+  delete(url: string, params: any) {
+    const config: any = {
+      method: "delete",
+      url: url,
+    };
+    if (params) {
+      config.params = params;
+    }
+    return request(config);
+  },
+};
+
+//导出
+export default http;
---
+++ src/utils/request.ts
@@ -0,0 +1,88 @@
+/*
+ * @Description: 封装axios方法
+ * @Author: 王振
+ * @Date: 2021-06-25 12:31:02
+ * @LastEditors: 王振
+ * @LastEditTime: 2021-06-25 12:32:56
+ */
+
+//导入axios
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+//导入vuex，获取token
+import store from "@/store";
+// vant的toast提示框组件
+import { Toast } from "vant";
+
+//1. 创建新的axios实例
+const instance = axios.create({
+  // 公共接口
+  baseURL: "http://localhost:3000",
+  // 超时时间 单位是ms，这里设置了10s的超时时间
+  timeout: 10000,
+});
+
+//2. 配置请求头，全局的 axios 默认值
+instance.defaults.headers.post["Content-Type"] = "application/json;charset=UTF-8"; // 流程组件 content-type
+
+//3.添加一个请求拦截器
+instance.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    //发请求前做的一些处理，数据转化，配置请求头，设置token,设置loading等
+    // 每次发送请求之前判断vuex中是否存在token,如果存在，则统一在http请求的header都加上token，这样后台根据token判断你的登录情况
+    const token = `Bearer ${store.getters.token}`;
+    if (token) {
+      config.headers["Authorization"] = token;
+    }
+    //设置loading
+    Toast.loading({
+      message: "加载中...",
+      duration: 0, //一直存在
+      forbidClick: true, //禁止点击
+    });
+    //数据转换,判断数据格式为formdata还是json格式
+    //json格式
+    config.data = JSON.stringify(config.data);
+    return config;
+  },
+  (error) => {
+    //出现请求错误，清除toast
+    Toast.clear();
+    Toast("请求错误，请稍后重试");
+    console.info("error: ");
+    console.info(error);
+    return Promise.reject(error);
+  }
+);
+
+//4. 添加一个响应拦截器
+instance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    const { status, data } = response;
+    if (status == 200) {
+      // 接口网络请求成功，关闭等待提示
+      Toast.clear();
+      if (data.code == 0) {
+        //接口请求结果正确
+        return data;
+      } else {
+        // Toast(data.message);
+        return Promise.reject(data);
+      }
+    }
+  },
+  (error: AxiosError) => {
+    //响应失败，关闭等待提示
+    Toast.clear();
+    // 超时处理
+    if (JSON.stringify(error).includes("Network Error")) {
+      Toast.fail("网络超时");
+    } else {
+      Toast.fail("服务器连接失败");
+    }
+    console.info(error);
+    return Promise.reject(error);
+  }
+);
+
+//5. 导出文件
+export default instance;
